8937001b07b1757e70ed5e4f1944c4e22cb0acd9
rossmartin <rmartin311@gmail.com>
2013-01-02 18:17:59 -0600

    Update www/js/bonjour.js
    fixed comments

--- www/js/bonjour.js
+++ www/js/bonjour.js
@@ -124,7 +124,7 @@ function resetBrowserPage(){ // called in onpagebeforeshow
     $("#browserPage").trigger("create"); // calling this in case markup needs enhanced
 }
 
-function changeBrowserPage(){ // called from NetServiceResolutionDelegate.m when json string is sent from sender
+function changeBrowserPage(){ // called from SocketClientDelegate.m when json string is sent from sender
     hideLoader();
     $("#deviceListview").empty();
     $("#deviceListview, #sendDataLoadIcon").hide();
@@ -139,7 +139,7 @@ function changeBrowserPage(){ // called from NetServiceResolutionDelegate.m when
     cordova.exec(stopServiceBrowserCB, stopServiceBrowserFail, 'BonjourPlugin', 'stopServiceBrowser', ['']);
 }
 
-function appendNetworkDevices(jsonEscaped){ // called from SocketClientDelegate.m when sending device finishes finding devices over Bonjour
+function appendNetworkDevices(jsonEscaped){ // called from NetServiceBrowserDelegate.m when sending device finishes finding devices over Bonjour
     console.log("appendNetworkDevices function is firing");
     console.log("jsonEscaped ====> " + jsonEscaped);
     var deviceJSON = decodeURIComponent(jsonEscaped);
